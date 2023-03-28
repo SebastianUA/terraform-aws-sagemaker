6141e9f87b6ba12ecb2671178cc05835fffd7953
Vitalii Natarov <solo.metalisebastian@gmail.com>
2023-03-28 23:10:55 +0200

    Dev (#12)
    * Update outputs.tf
    
    * Update README.md
    
    * Update README.md
    
    * Update main.tf

--- outputs.tf
+++ outputs.tf
@@ -181,10 +181,10 @@ output "sagemaker_feature_group_arn" {
   value       = element(concat(aws_sagemaker_feature_group.sagemaker_feature_group.*.arn, [""]), 0)
 }
 
-output "sagemaker_feature_group_name" {
-  description = "The name of the Feature Group."
-  value       = element(concat(aws_sagemaker_feature_group.sagemaker_feature_group.*.name, [""]), 0)
-}
+# output "sagemaker_feature_group_name" {
+#  description = "The name of the Feature Group."
+#  value       = element(concat(aws_sagemaker_feature_group.sagemaker_feature_group.*.name, [""]), 0)
+# }
 
 #---------------------------------------------------
 # AWS sagemaker code repository
